0474fbabeb3e3e7d67faadc09f3397da7a5bd425
PhatCa <phatca@hawaii.edu>
2023-04-07 01:22:14 -1000

    got the calculation woking

--- recipe/recipecard/models.py
+++ recipe/recipecard/models.py
@@ -9,7 +9,7 @@ class Recipe(models.Model):
     instructions = models.TextField()
     picture = models.CharField(max_length=255)
     author = models.ForeignKey(User, on_delete=models.PROTECT,default=None)
-
+    nutrient_info = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
--- recipe/recipecard/views.py
+++ recipe/recipecard/views.py
@@ -6,6 +6,9 @@ import json
 import requests
 # Create your views here.
 
+#iJlhrcEhWmmtl93uiqrUYcLQYoMLIPiesxI3mepv (api key)
+
+
 def recipe_list(request):
     recipe = Recipe.objects.all().order_by('id')
     return render(request,'recipecard/recipe_list.html',{'recipes':recipe})
@@ -20,11 +23,32 @@ def recipe_create(request):
     if request.method == 'POST':
         form = forms.CreateRecipe(request.POST)
         if form.is_valid():
+            # Convert ingredient string to array
+            ingredient_array = form.cleaned_data['ingredient'].split(", ")
+
+            nutrient_info = {}
+            for ingredient in ingredient_array:
+                response = requests.get(f"https://api.nal.usda.gov/fdc/v1/foods/search?api_key=iJlhrcEhWmmtl93uiqrUYcLQYoMLIPiesxI3mepv&query={ingredient}")
+                newData = response.json()
+                nutrients = newData['foods'][1]['foodNutrients']
+                for nutrient in nutrients:
+                    nutrient_name = nutrient['nutrientName']
+                    nutrient_value = nutrient['value']
+                    nutrient_unit = nutrient['unitName']
+                    if nutrient_name in nutrient_info:
+                        nutrient_info[nutrient_name]['value'] += nutrient_value
+                    else:
+                        nutrient_info[nutrient_name] = {'value': nutrient_value, 'unit': nutrient_unit}
+
+
             instance = form.save(commit=False)
             instance.author = request.user
+            instance.nutrient_info = nutrient_info  
             instance.save()
-            return redirect('recipes:list')
+            return render(request, 'recipecard/success.html', {'nutrient_info': nutrient_info})
 
-    else:    
-        form = forms.CreateRecipe
-    return render(request,'recipecard/recipe_create.html',{'form':form})
+
+
+    else:
+        form = forms.CreateRecipe()
+    return render(request, 'recipecard/recipe_create.html', {'form': form})
