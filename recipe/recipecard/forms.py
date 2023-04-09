e35b6a981912d01a494135a741a713581a1b3a0d
PhatCa <phatca@hawaii.edu>
2023-04-08 23:44:08 -1000

    styling create form

--- recipe/recipecard/forms.py
+++ recipe/recipecard/forms.py
@@ -5,4 +5,10 @@ class CreateRecipe(forms.ModelForm):
     class Meta:
         model = models.Recipe
         fields = ['name','ingredient','instructions','picture']
-        
+        widgets={
+            'name' : forms.TextInput(attrs={'class':'form-control'}),
+            'ingredient' : forms.TextInput(attrs={'class':'form-control'}),
+            'instructions' : forms.TextInput(attrs={'class':'form-control'}),
+            'picture' : forms.TextInput(attrs={'class':'form-control'}),
+
+        }
